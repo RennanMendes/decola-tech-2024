5e909034d382c282b04a15fd4a2937999e14b4f1
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-12-28 17:19:31 -0300

    criado: desafio de testes unitarios

---
+++ desafio-testes-unitarios/TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -0,0 +1,63 @@
+using TestesUnitarios.Desafio.Console.Services;
+
+namespace TestesUnitarios.Desafio.Tests;
+
+public class ValidacoesStringTests
+{
+    private ValidacoesString _validacoes = new ValidacoesString();
+
+    [Fact]
+    public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+    {
+        // Arrange
+        var texto = "matrix";
+
+        // Act
+        var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
+
+        // Assert
+        Assert.Equal(6, resultado);
+    }
+
+    [Fact]
+    public void DeveConterAPalavraQualquerNoTexto()
+    {
+        // Arrange
+        var texto = "Esse é um texto qualquer";
+        var textoProcurado = "qualquer";
+
+        // Act
+        var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
+
+        // Assert
+        Assert.True(resultado);
+    }
+
+    [Fact]
+    public void NaoDeveConterAPalavraTesteNoTexto()
+    {
+        // Arrange
+        var texto = "Esse é um texto qualquer";
+        var textoProcurado = "teste";
+
+        // Act
+        var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
+
+        // Assert
+        Assert.False(resultado);
+    }
+
+    [Fact]
+    public void TextoDeveTerminarComAPalavraProcurado()
+    {
+        // Arrange
+        var texto = "Começo, meio e fim do texto procurado";
+        var textoProcurado = "procurado";
+
+        // Act
+        var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
+
+        // Assert
+        Assert.True(resultado);
+    }
+}
